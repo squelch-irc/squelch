5e2fe298cfe350410e5c3e0bb17777653d388986
Rahat Ahmed <rahatarmanahmed@gmail.com>
2015-06-27 11:28:58 -0500

    Removed eslint and added jshint + jscs

--- app/mainApp.jsx
+++ app/mainApp.jsx
@@ -2,4 +2,4 @@ require('babel-core/register');
 
 import React from 'react';
 
-React.render(<h1>TODO: Implement Squelch</h1>, document.getElementById('squelch-root'))
+React.render(<h1>TODO: Implement Squelch</h1>, document.getElementById('squelch-root'));
--- scripts/build-release.js
+++ scripts/build-release.js
@@ -6,28 +6,28 @@ var _ = require('lodash');
 var platforms = argv.platform || argv.p || process.platform;
 var archs = argv.arch || argv.a || process.arch;
 
-_.each(platforms.split(','), function(platform){
-	_.each(archs.split(','), function(arch){
-		var packagerOpts = {
-			dir: __dirname + '/..',
-			name: 'Squelch-' + platform + '-' + arch,
-			asar: true,
-			version: pkg.devDependencies['electron-prebuilt'].match(/\d+\.\d+\.\d/)[0],
-			platform: platform,
-			arch: arch,
-			out: __dirname + '/../release',
-			prune: true,
-			ignore: [
-			'/test($|/)',
-			'/tools($|/)',
-			'/scripts($|/)',
-			'/release($|/)',
-			]
-			// TODO: add icon
-		};
-		packager(packagerOpts, function(err, path) {
-			if(err) { throw err; }
-			console.log('Successfully built package at ' + path);
-		});
-	});
+_.each(platforms.split(','), function(platform) {
+    _.each(archs.split(','), function(arch) {
+        var packagerOpts = {
+            dir: __dirname + '/..',
+            name: 'Squelch-' + platform + '-' + arch,
+            asar: true,
+            version: pkg.devDependencies['electron-prebuilt'].match(/\d+\.\d+\.\d/)[0],
+            platform: platform,
+            arch: arch,
+            out: __dirname + '/../release',
+            prune: true,
+            ignore: [
+            '/test($|/)',
+            '/tools($|/)',
+            '/scripts($|/)',
+            '/release($|/)'
+            ]
+            // TODO: add icon
+        };
+        packager(packagerOpts, function(err, path) {
+            if (err) { throw err; }
+            console.log('Successfully built package at ' + path);
+        });
+    });
 });
