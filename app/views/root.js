694ad16129eebd8c0e0543585397b91a280542bf
Kyle Kemp <kyle@seiyria.com>
2015-06-28 09:23:24 -0500

    adding some repeaters

--- app/views/root.js
+++ app/views/root.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {UserList} from './userlist';
 import {Chat} from './chat';
+import {Input} from './input';
 
 export class View extends React.Component {
   render() {
     return (
       <div>
-      <UserList /><Chat />
+      <UserList /><Chat /><Input />
       </div>
     );
   }
