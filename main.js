b39e79951f81c41403642b36c2171dd4da954877
Rahat Ahmed <rahatarmanahmed@gmail.com>
2015-06-28 12:01:33 -0500

    Fixed not quitting on mac & cleaned up main.js

--- main.js
+++ main.js
@@ -7,20 +7,18 @@ require('crash-reporter').start();
 var mainWindow = null;
 
 app.on('window-all-closed', function() {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+    app.quit();
 });
 
 app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        width: 1024,
+        height: 576,
+        "min-width": 768,
+        "min-height": 432
+    });
 
-    mainWindow = new BrowserWindow({width: 1024, height: 728});
-
-    if (process.env.HOT) {
-        mainWindow.loadUrl('file://' + __dirname + '/app/hot-dev-app.html');
-    } else {
-        mainWindow.loadUrl('file://' + __dirname + '/app/app.html');
-    }
+    mainWindow.loadUrl('file://' + __dirname + '/app/app.html');
 
     mainWindow.on('closed', function() {
         mainWindow = null;
@@ -29,5 +27,4 @@ app.on('ready', function() {
     if (process.env.NODE_ENV === 'development') {
         mainWindow.openDevTools();
     }
-
 });
