3ab65b027e0f73fb9732c13d394fad7a50e4b373
Rahat Ahmed <rahatarmanahmed@gmail.com>
2015-06-30 17:34:47 -0500

    Added Squelch global + config loader

--- app/scripts/mainApp.jsx
+++ app/scripts/mainApp.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import {View} from './views/root.js';
 import LessLoader from 'less-hot';
+import Squelch from './core/squelch'
+
+// Make Squelch global
+window.Squelch = Squelch;
+
+Squelch.config.read()
+.then(() => {
+    // Render view after loading the config
+    React.render(
+        <View />,
+        document.getElementById('squelch-root')
+    );
+});
 
 // Load our less styles
 var lessLoader = new LessLoader();
 document.querySelector('head').appendChild(lessLoader('./app/less/app.less'));
-
-React.render(
-	<View />,
-	document.getElementById('squelch-root')
-);
