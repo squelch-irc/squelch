48f3282d8b4991f4a831390b09abce9d3dcf7c30
Rahat Ahmed <rahatarmanahmed@gmail.com>
2015-07-02 00:31:01 -0500

    Updated electron-packager to 5.0.0, taking advantage of -all option

--- scripts/build-release.js
+++ scripts/build-release.js
@@ -3,31 +3,29 @@ var argv = require('minimist')(process.argv.slice(2));
 var pkg = require ('../package.json');
 var _ = require('lodash');
 
-var platforms = argv.platform || argv.p || process.platform;
-var archs = argv.arch || argv.a || process.arch;
+var platform = argv.platform || argv.p || process.platform;
+var arch = argv.arch || argv.a || process.arch;
 
-_.each(platforms.split(','), function(platform) {
-    _.each(archs.split(','), function(arch) {
-        var packagerOpts = {
-            dir: __dirname + '/..',
-            name: 'Squelch-' + platform + '-' + arch,
-            asar: true,
-            version: pkg.devDependencies['electron-prebuilt'].match(/\d+\.\d+\.\d/)[0],
-            platform: platform,
-            arch: arch,
-            out: __dirname + '/../release',
-            prune: true,
-            ignore: [
-            '/test($|/)',
-            '/tools($|/)',
-            '/scripts($|/)',
-            '/release($|/)'
-            ]
-            // TODO: add icon
-        };
-        packager(packagerOpts, function(err, path) {
-            if (err) { throw err; }
-            console.log('Successfully built package at ' + path);
-        });
+var packagerOpts = {
+    dir: __dirname + '/..',
+    name: 'Squelch',
+    asar: true,
+    version: pkg.devDependencies['electron-prebuilt'].match(/\d+\.\d+\.\d/)[0],
+    platform: platform,
+    arch: arch,
+    out: __dirname + '/../release',
+    prune: true,
+    ignore: [
+    '/test($|/)',
+    '/tools($|/)',
+    '/scripts($|/)',
+    '/release($|/)'
+    ]
+    // TODO: add icon
+};
+packager(packagerOpts, function(err, paths) {
+    if (err) { throw err; }
+    paths.forEach(function(path) {
+        console.log('Successfully built package at ' + path);
     });
 });
