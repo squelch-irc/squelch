241c5deb8d4e063387c47f5cc28c8265dd89cd81
Kyle Kemp <kyle@seiyria.com>
2015-07-08 18:24:01 -0500

    sort users by symbol

--- app/scripts/views/userlist.jsx
+++ app/scripts/views/userlist.jsx
@@ -8,11 +8,36 @@ export default class UserList extends React.Component {
         this.state = {
             users: [
                 {name: 'Seiyria', flag: '@'},
+                {name: 'Darkblizer', flag: '%'},
+                {name: 'Zesty', flag: '&'},
+                {name: 'Kurea', flag: '~'},
                 {name: 'KR', flag: '~'},
                 {name: 'Freek', flag: ''},
+                {name: 'Ergo', flag: '+'},
                 {name: 'Darkbuizel', flag: '+'}
             ]
         };
+    }
+
+    sortedUsers() {
+        var ranks = {
+            '~': 5, // owner
+            '&': 4, // admin
+            '@': 3, // op
+            '%': 2, // halfop
+            '+': 1, // voice
+            '': 0   // none
+        };
+
+        var x = _(this.state.users)
+            .sortBy((user) => {
+                return ranks[user.flag];
+            })
+            .reverse()
+            .value();
+
+        console.log(JSON.stringify(x));
+        return x;
     }
 
     render() {
@@ -21,7 +46,7 @@ export default class UserList extends React.Component {
                 <div className='userlist-title'>Users</div>
                 <ul className='userlist'>
                 {
-                    _.map(this.state.users, (user) => {
+                    _.map(this.sortedUsers(), (user) => {
                         return <li className='user' key={user.name}>
                             <span className='user-flag'>{user.flag}</span>
                             <span className='user-name'>{user.name}</span>
