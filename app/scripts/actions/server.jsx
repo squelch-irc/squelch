0192dc76e322fffccde7a17238455f7413680add
Rahat Ahmed <rahatarmanahmed@gmail.com>
2015-07-14 18:27:30 -0500

    Moved server creation to AddServerAction, triggered ServerEventAction

--- app/scripts/actions/server.jsx
+++ app/scripts/actions/server.jsx
@@ -1,10 +1,77 @@
+import ConfigStore from '../stores/config'
+
+import _ from 'lodash';
+import Client from 'squelch-client';
+
+// Options for squelch-client that are always the same
+// ...they just gotta be that way
+const HARDCODED_SERVER_OPTIONS = {
+    verbose: true,
+    verboseError: false,
+    autoNickChange: true,
+    autoSplitMessage: true,
+    messageDelay: 0,
+    stripColors: false,
+    stripStyles: false
+};
+
+// Options for squelch-client that can be set for all servers
+const APP_SERVER_OPTIONS = [
+    'autoRejoin',
+    'autoReconnect',
+    'autoReconnectTries',
+    'reconnectDelay',
+    'timeout'
+];
+
+// Default options for servers
+const DEFAULT_SERVER_OPTIONS = {
+    port: 6667,
+    nick: 'SquelchUser',
+    username: 'SquelchUser',
+    realname: 'SquelchUser',
+    channels: [],
+    ssl: false,
+    selfSigned: false,
+    certificateExpired: false
+};
+
 export var ServerEventAction = (context, payload, done) => {
     context.dispatch('SERVER_EVENT', payload);
     done();
 };
 
+/*
+payload.config requires
+    server
+    port
+    nick
+    username
+    realname
+    channels
+    ssl
+    selfSigned
+    certificateExpired
+*/
 export var AddServerAction = (context, payload, done) => {
-    context.dispatch('ADD_SERVER', payload);
+    let config = context.getStore(ConfigStore).getState().config;
+    let serverConfig = _.assign(
+        {},
+        DEFAULT_SERVER_OPTIONS,
+        payload.config,
+        HARDCODED_SERVER_OPTIONS
+    );
+    _.each(APP_SERVER_OPTIONS, (opt) => serverConfig[opt] = config[opt]);
+
+    let server = new Client(serverConfig);
+    server.onAny((data) => {
+        context.executeAction(ServerEventAction, {
+            type: server.event,
+            data
+        });
+    });
+
+    context.dispatch('ADD_SERVER', {server});
     done();
 };
 
--- app/scripts/stores/servers.jsx
+++ app/scripts/stores/servers.jsx
@@ -1,42 +1,5 @@
-import {ServerEventAction} from '../actions/server.jsx'
-import ConfigStore from '../stores/config'
-
-import Client from 'squelch-client';
 import _ from 'lodash';
 import {BaseStore} from 'fluxible/addons';
-
-// Options for squelch-client that are always the same
-// ...they just gotta be that way
-const HARDCODED_SERVER_OPTIONS = {
-    verbose: true,
-    verboseError: false,
-    autoNickChange: true,
-    autoSplitMessage: true,
-    messageDelay: 0,
-    stripColors: false,
-    stripStyles: false
-};
-
-// Options for squelch-client that can be set for all servers
-const APP_SERVER_OPTIONS = [
-    'autoRejoin',
-    'autoReconnect',
-    'autoReconnectTries',
-    'reconnectDelay',
-    'timeout'
-];
-
-// Default options for servers
-const DEFAULT_SERVER_OPTIONS = {
-    port: 6667,
-    nick: 'SquelchUser',
-    username: 'SquelchUser',
-    realname: 'SquelchUser',
-    channels: [],
-    ssl: false,
-    selfSigned: false,
-    certificateExpired: false
-};
 
 const MUTATION_EVENTS = [
     'connect',
@@ -65,38 +28,17 @@ class ServerStore extends BaseStore {
         };
     }
 
-    /*
-    config requires
-        server
-        port
-        nick
-        username
-        realname
-        channels
-        ssl
-        selfSigned
-        certificateExpired
-    */
     _addServer(payload) {
-        let config = this.dispatcher.getStore(ConfigStore).getState().config;
-        let serverConfig = _.assign({}, DEFAULT_SERVER_OPTIONS, payload.config, HARDCODED_SERVER_OPTIONS);
-        _.each(APP_SERVER_OPTIONS, (opt) => serverConfig[opt] = config[opt]);
-        var id = _.uniqueId('server');
-        var server = new Client(serverConfig);
+        let server = payload.server
+        let id = _.uniqueId('server');
         server.id = id;
         this.servers[id] = server;
 
         server.onAny((data) => {
-            // TODO: FIGURE OUT HOW TO EXECUTE THIS ACTION!!!
-            // this.getContext().executeAction(ServerEventAction, {
-            //     type: server.event,
-            //     data
-            // });
             if (_.contains(MUTATION_EVENTS, server.event)) {
                 this.emitChange();
             }
         });
-
         this.emitChange();
     }
 
