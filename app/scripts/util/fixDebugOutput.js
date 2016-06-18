e793dcf38c7119cec4e1531dab961abf511d377c
Rahat Ahmed <rahatarmanahmed@gmail.com>
2016-06-18 14:35:10 -0500

    Fix debug logs from squelch-client with stdout monkeypatch
    There's an electron bug that makes stdout/stderr not work in the renderer.

---
+++ app/scripts/util/fixDebugOutput.js
@@ -0,0 +1,14 @@
+module.exports = () => {
+    // Hook into process.stdout so that using the 'debug' module will work
+    // See https://github.com/electron/electron/issues/5051
+    if(process.env.DEBUG) {
+        // Gotta force debug to print to stdout instead of default stderr
+        process.env.DEBUG_FD = 1;
+
+        const oldWrite = process.stdout.write;
+        process.stdout.write = function(str, enc, fd) {
+            console.log(str);
+            oldWrite.call(process.stdout, str, enc, fd);
+        };
+    }
+}
