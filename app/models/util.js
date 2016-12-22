83f9cbde90e7f1a95d8894a61e5126d9c5c8b299
Rahat Ahmed <rahatarmanahmed@gmail.com>
2016-12-21 18:42:02 -0600

    Sort nick list, add nick hashing util

--- app/models/util.js
+++ app/models/util.js
@@ -1,7 +1,28 @@
+const hash = require('string-hash')
+
+const nickColorClasses = [
+  'dark-red',
+  'red',
+  'light-red',
+  'orange',
+  'gold',
+  'purple',
+  'light-purple',
+  'dark-pink',
+  'dark-green',
+  'green',
+  'navy',
+  'dark-blue',
+  'blue',
+  'light-blue'
+]
+
 module.exports = {
   getCurrentServerId: state => state.location.params.serverId,
   getCurrentChannel: state => state.location.params.channel,
   getCurrentPrivMsg: state => state.location.params.user,
+
+  nickColor: nick => nickColorClasses[hash(nick) % nickColorClasses.length],
 
   isActive: (state, serverId, channel, user) => {
     let active = state.location.params.serverId === String(serverId)
